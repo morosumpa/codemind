5ef809bfa23a5662b1b98ee4301fd380e4d615da
NRP tech <nrpcontacto@gmail.com>
2024-02-05 13:20:56 +0100

    tabla logros subida y no da error

---
+++ codemind/database/migrations/2024_02_05_121844_create_achievements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+// achievements migration file
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('type', ['bronze', 'silver', 'gold', 'platinum']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            // Add other columns as needed
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achievements');
+    }
+};
