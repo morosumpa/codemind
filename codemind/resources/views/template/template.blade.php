cd10cb42a13699cb0dd2958831d451508c51ee24
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-18 20:31:57 +0100

    añadir favicon y logo, cambiar login navbar

--- codemind/resources/views/template/template.blade.php
+++ codemind/resources/views/template/template.blade.php
@@ -34,6 +34,9 @@
     <!-- Iconos -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon" />
 </head>
 
 <body>
